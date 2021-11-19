b02ea6f5a1ab4e62bb6578e2a08ec344a2c56b5a
AlteredArt <cherokeedreemer@gmail.com>
2021-11-18 22:42:31 -0600

    next step

---
+++ objects/higherFunctions.js
@@ -0,0 +1,20 @@
+//ES9
+
+//Object spread operator
+const animals = {
+    tiger: 25,
+    lion: 5,
+    mokey: 2,
+    bird: 48
+}
+
+
+function objectSpread(p1, p2, p3) {
+    console.log('fn tiger:', p1)
+    console.log('fn lion:', p2)
+    console.log('fn rest:', p3)
+}
+
+const { tiger, lion, ...rest } = animals;
+
+objectSpread(animals)
