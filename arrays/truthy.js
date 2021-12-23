6e5e38770aca2541b826dec1234e369a968aac47
AlteredArt <cherokeedreemer@gmail.com>
2021-12-23 17:59:02 -0600

    xmas eve tomorrow

--- arrays/truthy.js
+++ arrays/truthy.js
@@ -1,10 +1,15 @@
-// takes an array and returns the number of truthy elements
-const array = [0,1,2,3, '', 6, null, false, true]
-function countTruthy(array) {
-  let count = 0
+// This function takes an array and returns the number of truthy elements
+const arrayMisc = [0,1,2,3, '', 6, null, false, true]
+
+function countTruthy(arrayMisc) {
+  let count = 0;
+
   for (let value of array)
    if (value)
+   //If value is true add one to count
     count ++;
+    
     console.log("true",count);
 }
- countTruthy(array)
+
+countTruthy(array);
